6155586c9cb6cc2440841ec374abd09a60c43539
Andrew Miracle <me@andrewmiracle.com>
2019-03-02 12:02:32 +0200

    direct open vpn implementation

---
+++ vpnbook/run.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+### RUN VPNBOOK VPN profiles
+### VPN SOURCE https://www.vpnbook.com/freevpn
+
+openvpn --config vpnbook-us2-udp25000.ovpn
+
+
+# username : vpnbook
+# password : 9vtw8e7
+
